58fa8b194bcad0c80a5da3a4d43388da53e41ffe
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-03-29 10:50:03 +0100

    Add violations stat
    Also remove output of density, as no clear results

--- nuc_analyze/nuc_analyze.py
+++ nuc_analyze/nuc_analyze.py
@@ -1,22 +1,30 @@
 from pathlib import Path
 from h5py import File as HDFFile
 import numpy as np
-
-def density(nuc, radius=1.0, structure="0"):
-    params = nuc['structures'][structure]['calculation'].attrs
-    radius = params['particle_sizes'][-1] ** params['scaling_exponent']
-
-    coords = np.concatenate(list(nuc['structures'][structure]['coords'].values()), axis=1)
-    neighbours = np.empty((coords.shape[0], coords.shape[1]), dtype='int')
-    for i, structure in enumerate(coords):
-        for j, coord in enumerate(structure):
-            neighbours[i, j] = np.count_nonzero(np.linalg.norm(structure - coord, axis=-1) < radius)
-
-    return np.mean(neighbours), np.std(neighbours)
+from collections import defaultdict
 
 def scale(nuc, structure="0"):
     coords = np.concatenate(list(nuc['structures'][structure]['coords'].values()), axis=1)
     return np.mean(np.std(coords, axis=1))
+
+def violations(nuc, structure="0"):
+    coords = nuc['structures'][structure]['coords']
+    restraints = nuc['structures'][structure]['restraints']
+    violations = defaultdict(int)
+
+    for chr_a, b in restraints.items():
+        for chr_b, restraints in b.items():
+            if not len(restraints) > 0:
+                continue
+            a_coords = coords[chr_a][:][:, restraints['indices'][:, 0]]
+            b_coords = coords[chr_b][:][:, restraints['indices'][:, 1]]
+            dist = np.linalg.norm(a_coords - b_coords, axis=-1)
+            viol = ((restraints['dists'][:, 1] < dist) |
+                    (restraints['dists'][:, 0] > dist))
+            for i, structure_violations in enumerate(viol):
+                violations[i] += structure_violations.sum()
+
+    return np.mean(list(violations.values())), np.std(list(violations.values()))
 
 def main(args=None):
     from argparse import ArgumentParser
@@ -27,15 +35,13 @@ def main(args=None):
     parser.add_argument("nucs", type=Path, nargs='+', help="The files to analyze")
     parser.add_argument("--structure", type=str, default="0",
                         help="Which structure in the file to use")
-    parser.add_argument("--radius", type=float, default=1.0,
-                        help="Number of bead-radii to consider for density")
     parser.add_argument("--params", type=str, nargs='*', default=[],
                         help="Which calculation parameters to print")
 
     args = parser.parse_args(argv[1:] if args is None else args)
 
     writer = csv.DictWriter(stdout, [
-        "filename", "density_radius", "std", "density_mean", "density_std"
+        "filename", "scale", "violations_mean", "violations_std"
     ] + args.params)
     writer.writeheader()
     for nuc in args.nucs:
@@ -44,12 +50,13 @@ def main(args=None):
             params = {k: params[k] for k in args.params}
             if "particle_sizes" in params:
                 params["particle_sizes"] = params["particle_sizes"][-1]
+            params["filename"] = str(nuc.name)
 
-            params["filename"] = str(nuc.name).split(".")[0]
-            params["density_radius"] = args.radius
+            params["scale"] = scale(f, args.structure)
+            params["violations_mean"], params["violations_std"] = (
+                violations(f, args.structure)
+            )
 
-            params["std"] = scale(f, args.structure)
-            params["density_mean"], params["density_std"] = density(f, args.radius, args.structure)
             writer.writerow(params)
 
 if __name__ == "__main__":
