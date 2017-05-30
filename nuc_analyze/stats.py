c959697586ece5b52b9d193bde59e93e49ff43c8
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-05-30 17:36:00 +0100

    Allow padding when counting violations

--- nuc_analyze/stats.py
+++ nuc_analyze/stats.py
@@ -20,7 +20,7 @@ def scale(nuc, structure="0"):
     coords = np.concatenate(list(nuc['structures'][structure]['coords'].values()), axis=1)
     return np.mean(np.std(coords, axis=1), axis=1)
 
-def violations(nuc, structure="0"):
+def violations(nuc, structure="0", padding=0.0):
     coords = nuc['structures'][structure]['coords']
     restraints = nuc['structures'][structure]['restraints']
     violations = Counter()
@@ -31,8 +31,8 @@ def violations(nuc, structure="0"):
         a_coords = coords[chr_a][:][:, restraints['indices'][:, 0]]
         b_coords = coords[chr_b][:][:, restraints['indices'][:, 1]]
         dist = np.linalg.norm(a_coords - b_coords, axis=-1)
-        viol = ((restraints['dists'][:, 1] < dist) |
-                (restraints['dists'][:, 0] > dist))
+        viol = ((restraints['dists'][:, 1] * (1.0 + padding) < dist) |
+                (restraints['dists'][:, 0] * (1.0 - padding) > dist))
         for model, model_violations in enumerate(viol):
             violations[model] += model_violations.sum()
     # Order of models doesn't matter, summarized anyway
@@ -42,13 +42,15 @@ def violations(nuc, structure="0"):
 @click.argument("nucs", type=Path, nargs=-1, required=True)
 @click.option("--structure", default="0", help="Which structure in the file to read")
 @click.option("--param", multiple=True, help="Which calculation parameters to print")
-def stats(nucs, structure, param):
+@click.option("--violation-padding", type=float, default=0.0,
+              help="How much (relative) a restraint may be violated by")
+def stats(nucs, structure, param, violation_padding):
     import csv
     from sys import stdout
 
-    stat_names = ["scale", "violations"]
+    stat_names = [("scale", {}), ("violations", {'padding': violation_padding})]
     stat_cols = list(chain.from_iterable(
-        ("{}_mean".format(s), "{}_std".format(s)) for s in stat_names
+        ("{}_mean".format(s), "{}_std".format(s)) for s, _ in stat_names
     ))
 
     writer = csv.DictWriter(stdout, ["filename"] + stat_cols + list(param))
@@ -61,8 +63,8 @@ def stats(nucs, structure, param):
                 params["particle_sizes"] = params["particle_sizes"][-1]
             params["filename"] = str(nuc.name)
 
-            for stat in stat_names:
-                stat_values = globals()[stat](f, structure)
+            for stat, kwargs in stat_names:
+                stat_values = globals()[stat](f, structure, **kwargs)
                 params["{}_mean".format(stat)] = np.mean(stat_values)
                 params["{}_std".format(stat)] = np.std(stat_values)
             writer.writerow(params)
