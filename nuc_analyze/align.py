2eac6eb9018ff02a91f18a0b24b213751bac6efd
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-09-20 12:04:10 +0100

    Allow specifying mirroring to 'align'
    Better for comparison.

--- nuc_analyze/align.py
+++ nuc_analyze/align.py
@@ -12,11 +12,12 @@ from .main import cli
 @cli.command()
 @click.argument("nuc", type=Path, required=True)
 @click.option("--structure", default="0", help="Which structure in the file to read")
-def align(nuc, structure):
+@click.option("--mirror/--no-mirror", default=True, help="Also align mirror images")
+def align(nuc, structure, mirror=True):
     with HDFFile(nuc, "r+") as f:
         coordss = f['structures'][structure]['coords']
         all_coords = np.concatenate(list(coordss.values()), axis=1)
         ref = np.median(all_coords, axis=0)
-        xforms = list(map(partial(least_squares, ref), all_coords))
+        xforms = list(map(partial(least_squares, ref, mirror=mirror), all_coords))
         for chr, coords in coordss.items():
             coords[:] = np.array(list(map(op.matmul, xforms, coords)), dtype=coords.dtype)
